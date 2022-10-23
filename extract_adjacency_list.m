314cd74bc7d9c1c52391c99322adc93463a4f848
Caden Kroonenberg <cskroonenberg@gmail.com>
2022-10-23 01:10:30 -0500

    Add edges between ways

--- extract_adjacency_list.m
+++ extract_adjacency_list.m
@@ -34,8 +34,103 @@ function [adjacency_list] = extract_adjacency_list(parsed_osm)
 		adjacency_list = connect_way(w_id, parsed_osm, adjacency_list, true);
 	end
 
-	% test node w/ degree 2
-	adjacency_list = add_edge(7879, 5208, nodes, adjacency_list);
+	% Connect Start to Hole 1
+	nd_1_id = way_end_nd_id(772145420, ways); % Final node of Start
+	nd_1_idx = nd_id_2_idx(nd_1_id, nodes);
+
+	nd_2_id = way_start_nd_id(773682303, ways); % First node of Hole 1
+	nd_2_idx = nd_id_2_idx(nd_2_id, nodes);
+
+	adjacency_list = add_edge(nd_1_idx, nd_2_idx, nodes, adjacency_list);
+
+	% Connect Hole 1 to Hole 2, 3
+	nd_1_id = way_end_nd_id(773682303, ways); % Final node of Hole 1
+	nd_1_idx = nd_id_2_idx(nd_1_id, nodes);
+
+	nd_2_id = way_start_nd_id(783638177, ways); % First node of Hole 2, 3
+	nd_2_idx = nd_id_2_idx(nd_2_id, nodes);
+
+	adjacency_list = add_edge(nd_1_idx, nd_2_idx, nodes, adjacency_list);
+
+	% Connect Hole 2, 3 to Bridge
+	% Hole 2, 3 Final Node = Bridge First Node
+
+	% Connect Bridge to Loop 1
+	nd_1_id = way_end_nd_id(867486266, ways); % Final node of Bridge
+	nd_1_idx = nd_id_2_idx(nd_1_id, nodes);
+
+	nd_2_id = way_end_nd_id(867486263, ways); % Final node of Loop 1 (Loop 1 nodes added in reverse order)
+	nd_2_idx = nd_id_2_idx(nd_2_id, nodes);
+
+	adjacency_list = add_edge(nd_1_idx, nd_2_idx, nodes, adjacency_list);
+
+	% Connect Loop 1 to Loop 2
+	% Loop 1 First Node = Loop 2 First Node
+
+	% Connect Loop 2 to midpoint node to Loop 3
+	nd_1_id = way_end_nd_id(777884654, ways); % Final node of Loop 2
+	nd_1_idx = nd_id_2_idx(nd_1_id, nodes);
+
+	mid_nd_id = 8085848420;
+	mid_nd_idx = nd_id_2_idx(mid_nd_id, nodes);
+
+	adjacency_list = add_edge(nd_1_idx, mid_nd_idx, nodes, adjacency_list);
+
+	nd_2_id = way_start_nd_id(777884652, ways); % First node of Loop 3
+	nd_2_idx = nd_id_2_idx(nd_2_id, nodes);
+
+	adjacency_list = add_edge(mid_nd_idx, nd_2_idx, nodes, adjacency_list);
+
+	% Connect Loop 3 to Loop 4
+	% Loop 3 Final Node = Loop 4 First Node
+
+	% Connect Loop 4 to Bridge
+	nd_1_id = way_end_nd_id(867486264, ways); % Final node of Loop 4
+	nd_1_idx = nd_id_2_idx(nd_1_id, nodes);
+
+	nd_2_id = way_end_nd_id(867486266, ways); % Final node of Bridge (Going reverse order now)
+	nd_2_idx = nd_id_2_idx(nd_2_id, nodes);
+
+	adjacency_list = add_edge(nd_1_idx, nd_2_idx, nodes, adjacency_list);
+
+	% Connect Bridge to Hole 4
+	nd_1_id = way_start_nd_id(867486266, ways); % First node of Bridge
+	nd_1_idx = nd_id_2_idx(nd_1_id, nodes);
+
+	nd_2_id = way_start_nd_id(772700736, ways); % First node of Hole 4
+	nd_2_idx = nd_id_2_idx(nd_2_id, nodes);
+
+	adjacency_list = add_edge(nd_1_idx, nd_2_idx, nodes, adjacency_list);
+
+	% Connect Hole 4 to Hole 5
+	% Hole 4 Final Node = Hole 5 First Node
+
+	% Connect Hole 5 to Hole 6
+	nd_1_id = way_end_nd_id(772737413, ways); % Final node of Hole 5
+	nd_1_idx = nd_id_2_idx(nd_1_id, nodes);
+
+	nd_2_id = way_start_nd_id(775243251, ways); % First node of Hole 6
+	nd_2_idx = nd_id_2_idx(nd_2_id, nodes);
+
+	adjacency_list = add_edge(nd_1_idx, nd_2_idx, nodes, adjacency_list);
+
+	% Connect Hole 6 to Practice Green
+	nd_1_id = way_end_nd_id(775243251, ways); % Final node of Hole 6
+	nd_1_idx = nd_id_2_idx(nd_1_id, nodes);
+
+	nd_2_id = way_start_nd_id(773154737, ways); % First node of Practice Green
+	nd_2_idx = nd_id_2_idx(nd_2_id, nodes);
+
+	adjacency_list = add_edge(nd_1_idx, nd_2_idx, nodes, adjacency_list);
+
+	% Connect Practice Green to Hole 7, 8, 9
+	nd_1_id = way_end_nd_id(773154737, ways); % Final node of Practice Green
+	nd_1_idx = nd_id_2_idx(nd_1_id, nodes);
+
+	nd_2_id = way_start_nd_id(773166141, ways); % First node of Hole 7, 8, 9
+	nd_2_idx = nd_id_2_idx(nd_2_id, nodes);
+
+	adjacency_list = add_edge(nd_1_idx, nd_2_idx, nodes, adjacency_list);
 
 end % extract_adjacency_list
 
@@ -124,3 +219,22 @@ function [way_idx] = way_id_2_idx(way_id, ways)
 	%disp('way_id_2_idx');
 	way_idx = find(ways.id(:) == way_id);
 end % way_id_2_idx
+
+function [start_nd_id] = way_start_nd_id(way_id, ways)
+%WAY_START_ND_ID Return a way's first node ID
+	%dist('way_start_nd')
+	way_idx = way_id_2_idx(way_id, ways);
+	way_nd_ids = ways.nd(way_idx);
+	way_nd_ids = way_nd_ids{:};
+	start_nd_id = way_nd_ids(1);
+end % way_start_nd_id
+
+function [end_nd_id] = way_end_nd_id(way_id, ways)
+%WAY_END_ND_ID Return a way's final node ID
+	%dist('way_start_nd')
+	way_idx = way_id_2_idx(way_id, ways);
+	way_nd_ids = ways.nd(way_idx);
+	way_nd_ids = way_nd_ids{:};
+	num_nd = size(way_nd_ids, 2);
+	end_nd_id = way_nd_ids(num_nd);
+end % way_end_nd_id
