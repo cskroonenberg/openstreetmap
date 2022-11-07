e71fd683a924bb7ecac98f1f896578847ab0a89b
Caden Kroonenberg <cskroonenberg@gmail.com>
2022-11-06 23:54:45 -0600

    Add node between ways for more precise map representation

--- extract_adjacency_list.m
+++ extract_adjacency_list.m
@@ -9,28 +9,58 @@ function [adjacency_list, nodes, ways] = extract_adjacency_list(parsed_osm)
 
 	n = size(nodes.id, 2)
 	max_nd_id = max(nodes.id);
-	%% new_node(nd_id, pred_id, nd_1_id, portion_nd_1, nd_2_id, way_id, nodes, ways) %%
+	%% new_node(nd_id, pred_id, nd_1_id, nd_2_id, lat_prop, lon_prop, way_id, nodes, ways) %%
 	%% Start -> Hole 1
-	[nodes, ways] = new_node(max_nd_id+1, 7221695450, 7221695450, 0.75, 7221695447, 772145420, nodes, ways);
-	[nodes, ways] = new_node(max_nd_id+2, max_nd_id+1, 7221695450, 0.50, 7221695447, 772145420, nodes, ways);
-	[nodes, ways] = new_node(max_nd_id+3, max_nd_id+2, 7221695450, 0.25, 7221695447, 772145420, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+1, 7221695450, 7221695450, 7221695447, 0.75, 0.75, 772145420, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+2, max_nd_id+1, 7221695450, 7221695447, 0.50, 0.50, 772145420, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+3, max_nd_id+2, 7221695450, 7221695447, 0.25, 0.25, 772145420, nodes, ways);
 
 	%% Hole 1 -> Hole 2, 3
-	[nodes, ways] = new_node(max_nd_id+4, 7319464063, 7319464063, 0.25, 7212354780, 773682303, nodes, ways);
-
-	[lat, ~] = avg_position(max_nd_id+4, 0.5, 7319464063, nodes);
-	[~, lon] = nd_xy(nd_id_2_idx(max_nd_id+4, nodes), nodes)
-	ways = insert_node_to_way(773682303, max_nd_id+5, 7319464063, ways);
-	nodes = insert_node_to_nodes(max_nd_id+5, lat, lon, nodes);
-
-	[nodes, ways] = new_node(max_nd_id+6, 7319464063, max_nd_id+5, 0.5, 7319464063, 773682303, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+4, 7319464063, 7319464063, 7212354780, 0.75, 0, 773682303, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+5, max_nd_id+4, 7319464063, 7212354780, 0.5, 0, 773682303, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+6, max_nd_id+5, 7319464063, 7212354780, 0.25, 0, 773682303, nodes, ways);
 
 	%% Loop
-	[nodes, ways] = new_node(max_nd_id+7, 7259781130, 7259781130, 0.5, 8085848420, 777884654, nodes, ways);
-	[nodes, ways] = new_node(max_nd_id+8, max_nd_id+7, 7259781136, 0.5, 8085848420, 777884654, nodes, ways);
-	[nodes, ways] = new_node(max_nd_id+9, max_nd_id+8, 7259781136, 0.5, max_nd_id+8, 777884654, nodes, ways);
-	[nodes, ways] = new_node(max_nd_id+10, max_nd_id+7, max_nd_id+7, 0.5, max_nd_id+8, 777884654, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+7, 7259781130, 7259781130, 8085848420, 0.5, 0.5, 777884654, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+8, max_nd_id+7, 7259781136, 8085848420, 0.5, 0.5, 777884654, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+9, max_nd_id+8, 7259781136, max_nd_id+8, 0.5, 0.5, 777884654, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+10, max_nd_id+7, max_nd_id+7, max_nd_id+8, 0.5, 0.75, 777884654, nodes, ways);
 
+
+	%% Bridge
+	[nodes, ways] = new_node(max_nd_id+11, 7212365275, 7212365275, 7212365276, 0.05, 0.05, 867486266, nodes, ways);
+	for i=1:18
+		[nodes, ways] = new_node(max_nd_id+11+i, max_nd_id+10+i, 7212365275, 7212365276, (0.05)*(i+1), (0.05)*(i+1), 867486266, nodes, ways);
+	end
+
+	%% Hole 2, 3
+	[nodes, ways] = new_node(max_nd_id+30, 7212365212, 7212365212, 7212365213, 0.66, 0.66, 783638177, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+31, max_nd_id+30, 7212365212, 7212365213, 0.33, 0.33, 783638177, nodes, ways);
+
+
+	%% Hole 4
+	[nodes, ways] = new_node(max_nd_id+32, 7214033538, 7214033538, 7214033537, 0.66, 0.66, 772700736, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+33, max_nd_id+32, 7214033538, 7214033537, 0.33, 0.33, 772700736, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+34, 7214033501, 7214033501, 7214033500, 0.5, 0.5, 772700736, nodes, ways);
+
+	%% Hole 5 -> Hole 6
+	[nodes, ways] = new_node(max_nd_id+35, 7214451422, 7214451422, 7215346089, 0.65, 0, 772737413, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+36, max_nd_id+35, max_nd_id+35, 7232770850, 0.5, 0.66, 772737413, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+37, max_nd_id+36, max_nd_id+36, 7232770850, 0.5, 0.8, 772737413, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+38, 7214451422, 7214451422, max_nd_id+35, 0.5, 0, 772737413, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+39, max_nd_id+35, max_nd_id+35, max_nd_id+36, 0.5, 0.8, 772737413, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+40, max_nd_id+37, max_nd_id+37, 7232770847, 0.5, 0.5, 772737413, nodes, ways);
+
+	%% Hole 6 -> Practice Green
+	[nodes, ways] = new_node(max_nd_id+41, 7218855994, 7218855994, 7218796926, 1, 0.66, 775243251, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+42, max_nd_id+41, max_nd_id+41, 7218796926, 0.2, 0.5, 775243251, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+43, max_nd_id+42, max_nd_id+42, 7218796936, 0, 0.75, 775243251, nodes, ways);
+
+	%% Practice Green -> Hole 7, 8, 9
+	[nodes, ways] = new_node(max_nd_id+44, 7218796951, 7218796951, 7221695418, 0.66, 0.66, 773154737, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+45, max_nd_id+44, max_nd_id+44, 7218921089, 0.75, 0.75, 773154737, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+46, max_nd_id+45, max_nd_id+45, 7218921089, 0.5, 0.5, 773154737, nodes, ways);
+	[nodes, ways] = new_node(max_nd_id+47, max_nd_id+46, max_nd_id+46, 7218921089, 0.25, 0.25, 773154737, nodes, ways);
 
 	n = size(nodes.id, 2)
 	adjacency_list = cell(1, n);
@@ -197,7 +227,7 @@ function [adjacency_list] = add_edge(nd_1_idx, nd_2_idx, nodes, adjacency_list)
 	%disp('add_edge');
 	if ~(nd_1_idx == nd_2_idx)
 		[dist, d_x, d_y] = edge_weight(nd_1_idx, nd_2_idx, nodes);
-		adjacency_list{nd_1_idx} = [adjacency_list{nd_1_idx}, {[nd_2_idx, dist, d_x, d_y]}];
+		adjacency_list{nd_1_idx} = [adjacency_list{nd_1_idx}; {[nd_2_idx, dist, d_x, d_y]}];
 	end
 end % add_edge
 
@@ -266,13 +296,13 @@ function [end_nd_id] = way_end_nd_id(way_id, ways)
 	end_nd_id = way_nd_ids(num_nd);
 end % way_end_nd_id
 
-function [lat, lon] = avg_position(nd_1_id, portion_nd_1, nd_2_id, nodes)
+function [lat, lon] = avg_position(nd_1_id, nd_2_id, lat_prop, lon_prop, nodes)
 %AVG_POSITION Calculate the average lat, lon position between two points based on a certain proportion
 	nd_1_idx = nd_id_2_idx(nd_1_id, nodes);
 	nd_2_idx = nd_id_2_idx(nd_2_id, nodes);
 
-	lat = portion_nd_1*nodes.xy(1, nd_1_idx) + (1-portion_nd_1)*nodes.xy(1, nd_2_idx);
-	lon = portion_nd_1*nodes.xy(2, nd_1_idx) + (1-portion_nd_1)*nodes.xy(2, nd_2_idx);
+	lat = lat_prop*nodes.xy(1, nd_1_idx) + (1-lat_prop)*nodes.xy(1, nd_2_idx);
+	lon = lon_prop*nodes.xy(2, nd_1_idx) + (1-lon_prop)*nodes.xy(2, nd_2_idx);
 end % avg_position
 
 function [ways] = insert_node_to_way(way_id, nd_id, predecessor_nd_id, ways)
@@ -290,20 +320,20 @@ function [ways] = insert_node_to_way(way_id, nd_id, predecessor_nd_id, ways)
 	disp('Node inserted.');
 end % insert_node_to_way
 
-function [nodes] = insert_node_to_nodes(nd_id, nd_lat, nd_lon, nodes)
+function [nodes] = insert_node_to_nodes(nd_id, nd_lon, nd_lat, nodes)
 %INSERT_NODE_TO_WAY Insert node into nodes with a given ID, lat, lon
 	nodes.id = [nodes.id, nd_id];
 	disp(['Added node ID ', num2str(nd_id), ' to nodes.id']);
-	nd_coord = [nd_lat, nd_lon];
+	nd_coord = [nd_lon, nd_lat];
 	nodes.xy = cat(2, nodes.xy, nd_coord.');
 	disp('Added node lat, lon to nodes.xy');
 end % insert_node_to_way
 
-function [nodes, ways] = new_node(nd_id, pred_id, nd_1_id, portion_nd_1, nd_2_id, way_id, nodes, ways)
+function [nodes, ways] = new_node(nd_id, pred_id, nd_1_id, nd_2_id, lon_prop, lat_prop, way_id, nodes, ways)
 %NEW_NODE Creates new node based on proportional position and predecessor for way placement, inserts node to ways and nodes
 	lat = 0;
 	lon = 0;
-	[lat, lon] = avg_position(nd_1_id, portion_nd_1, nd_2_id, nodes);
+	[lat, lon] = avg_position(nd_1_id, nd_2_id, lon_prop, lat_prop, nodes);
 	disp(['Node ', num2str(nd_id), ' lat: ', num2str(lat), ' lon: ', num2str(lon)]);
 	ways = insert_node_to_way(way_id, nd_id, pred_id, ways);
 	nodes = insert_node_to_nodes(nd_id, lat, lon, nodes);
