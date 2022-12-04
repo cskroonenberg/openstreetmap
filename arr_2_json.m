d95c8afc709dcabcfc659ad561ac384faf0f34e2
Caden Kroonenberg <cskroonenberg@gmail.com>
2022-12-04 16:35:44 -0600

    Make proper JSON filename a fn caller responsibility for arr_2_json.m

--- arr_2_json.m
+++ arr_2_json.m
@@ -1,11 +1,8 @@
-function [adjacency_list, nodes, ways] = arr_2_json(arr, arr_name, json_name)
+function [adjacency_list, nodes, ways] = arr_2_json(arr, arr_name, json_filename)
 %EXTRACT_ADJACENCY_LIST Parse OSM file to extract adjacency list containing node connections
 
     for y=1:size(arr)
-        current_dir = '/home/c699k482/eecs581/openstreetmap'; %TODO: Find path with pwd 
-        filename= sprintf('%s/%s.json', current_dir, json_name);
-
-        fid=fopen(filename,'w');
+        fid=fopen(json_filename,'w');
         s = struct(arr_name, arr); 
         encoded = jsonencode(s); 
         fprintf(fid, encoded); 
